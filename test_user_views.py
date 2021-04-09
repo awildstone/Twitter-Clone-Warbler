3b87f7b81b82dc99584ef97f6b258090fe1d840d
Aimee Wildstone <11568530+awildstone@users.noreply.github.com>
2021-04-08 20:53:25 -0700

    add user view tests

--- test_user_views.py
+++ test_user_views.py
@@ -3,7 +3,7 @@
 
 import os
 from unittest import TestCase
-from models import db, connect_db, Message, User
+from models import db, connect_db, Message, User, Follows
 
 #set DB environment to test DB
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
@@ -65,3 +65,186 @@ class UserViewTestCase(TestCase):
             self.assertIn(b'@testuser2', res.data)
             self.assertNotIn(b'@ilovecats', res.data)
             self.assertNotIn(b'@birbsrcool', res.data)
+    
+    def test_show_user_profile(self):
+        """ Does users_show(user_id) display a user's profile? """
+
+        with self.client as c:
+            res = c.get(f"/users/{self.user3.id}")
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(b'@ilovecats', res.data)
+    
+    def test_view_user_following(self):
+        """ Can authed user view list of users being followed by a user? """
+        #setup test follows
+        f1 = Follows(user_being_followed_id=self.user2.id, user_following_id=self.user1.id)
+        f2 = Follows(user_being_followed_id=self.user3.id, user_following_id=self.user1.id)
+        f3 = Follows(user_being_followed_id=self.user4.id, user_following_id=self.user1.id)
+
+        db.session.add_all([f1, f2, f3])
+        db.session.commit()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.user1.id
+            
+            res = c.get(f"/users/{self.user1.id}/following")
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(b'@testuser2', res.data)
+            self.assertIn(b'@ilovecats', res.data)
+            self.assertIn(b'@birbsrcool', res.data)
+    
+    def test_view_user_following_not_auth(self):
+        """ Does show_following(user_id) prevent unauthed users from vieweing the user's follow list? """
+
+        #setup test follows
+        f1 = Follows(user_being_followed_id=self.user2.id, user_following_id=self.user1.id)
+        f2 = Follows(user_being_followed_id=self.user3.id, user_following_id=self.user1.id)
+        f3 = Follows(user_being_followed_id=self.user4.id, user_following_id=self.user1.id)
+
+        db.session.add_all([f1, f2, f3])
+        db.session.commit()
+
+        with self.client as c:
+            res = c.get(f"/users/{self.user1.id}/following", follow_redirects=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(b'Access unauthorized.', res.data)
+
+
+    def test_view_user_followers(self):
+        """ Can authed user view a user's followers? """
+
+        #setup test follows
+        f1 = Follows(user_being_followed_id=self.user1.id, user_following_id=self.user2.id)
+        f2 = Follows(user_being_followed_id=self.user1.id, user_following_id=self.user3.id)
+        f3 = Follows(user_being_followed_id=self.user1.id, user_following_id=self.user4.id)
+
+        db.session.add_all([f1, f2, f3])
+        db.session.commit()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.user1.id
+            
+            res = c.get(f"/users/{self.user1.id}/followers")
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(b'@testuser2', res.data)
+            self.assertIn(b'@ilovecats', res.data)
+            self.assertIn(b'@birbsrcool', res.data)
+    
+
+    def test_view_user_followers_not_auth(self):
+        """ Does users_followers(user_id) prevent an unauthed user from viewing the user's followers? """
+
+        #setup test follows
+        f1 = Follows(user_being_followed_id=self.user1.id, user_following_id=self.user2.id)
+        f2 = Follows(user_being_followed_id=self.user1.id, user_following_id=self.user3.id)
+        f3 = Follows(user_being_followed_id=self.user1.id, user_following_id=self.user4.id)
+
+        db.session.add_all([f1, f2, f3])
+        db.session.commit()
+
+        with self.client as c:
+            res = c.get(f"/users/{self.user1.id}/followers", follow_redirects=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(b'Access unauthorized.', res.data)
+
+
+    def test_add_follow(self):
+        """ Can an authed user add a new follow? """
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.user1.id
+            
+            res = c.post("/users/follow/45", data={}, follow_redirects=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(len(Follows.query.all()), 1)
+            self.assertIn(b"@birbsrcool", res.data)
+
+    
+    def test_add_follow_no_auth(self):
+        """ Does add_follow(follow_id) prevent an unauthed user from adding a new follow? """
+
+        with self.client as c:
+
+            res = c.post("/users/follow/45", data={}, follow_redirects=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(b"Access unauthorized.", res.data)
+
+    def test_stop_follow(self):
+        """ Can an authed user stop following a user? """
+
+        #setup test follows
+        f1 = Follows(user_being_followed_id=self.user2.id, user_following_id=self.user1.id)
+        f2 = Follows(user_being_followed_id=self.user3.id, user_following_id=self.user1.id)
+        f3 = Follows(user_being_followed_id=self.user4.id, user_following_id=self.user1.id)
+
+        db.session.add_all([f1, f2, f3])
+        db.session.commit()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.user1.id
+            
+            res = c.post("/users/stop-following/31", data={}, follow_redirects=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(len(Follows.query.all()), 2)
+            self.assertIn(b"@testuser2", res.data)
+            self.assertIn(b"@birbsrcool", res.data)
+            self.assertNotIn(b"@ilovecats", res.data)
+
+
+    def test_stop_follow_no_auth(self):
+        """ Does stop_following(follow_id) prevent an unauthed user from un-following a user? """
+
+        #setup test follows
+        f1 = Follows(user_being_followed_id=self.user2.id, user_following_id=self.user1.id)
+        f2 = Follows(user_being_followed_id=self.user3.id, user_following_id=self.user1.id)
+        f3 = Follows(user_being_followed_id=self.user4.id, user_following_id=self.user1.id)
+
+        db.session.add_all([f1, f2, f3])
+        db.session.commit()
+
+        with self.client as c:
+            
+            res = c.post("/users/stop-following/31", data={}, follow_redirects=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(len(Follows.query.all()), 3)
+            self.assertIn(b"Access unauthorized.", res.data)
+
+    def test_edit_profile(self):
+        """ Can an authed user edit profile? """
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.user1.id
+            
+            res = c.post("/users/profile", data={"username": "im#1", "email": "num@one.com", "image_url": "/static/images/default-pic.png", "header_image_url": "/static/images/warbler-hero.jpg", "bio": "I'm #1!!", "location": "Denver", "password": "testuser1"}, follow_redirects=True)
+            
+            updated_user = User.query.get(self.user1.id)
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(updated_user.username, "im#1")
+            self.assertEqual(updated_user.email, "num@one.com")
+
+    
+    def test_edit_profile_no_auth(self):
+        """ Is an unauthed user prevented from editing a profile? """
+
+        with self.client as c:
+            res = c.post("/users/profile", data={"username": "im#1", "email": "num@one.com", "image_url": "/static/images/default-pic.png", "header_image_url": "/static/images/warbler-hero.jpg", "bio": "I'm #1!!", "location": "Denver", "password": "testuser1"}, follow_redirects=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(b"Access unauthorized.", res.data)
+    
+    def test_user_add_like(self):
+        """ Can an authed user add a new like? """
